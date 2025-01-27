f92b58055e93fe8c59e92138804e69eebf1fc4eb
Jedsadaporn-tnk <67319010003@tnk.ac.th>
2025-01-27 13:38:54 +0700

    weekproject

--- project/dataapi.py
+++ project/dataapi.py
@@ -3,17 +3,17 @@ mycursor = database.mydb.cursor()
 import fastapi
 app = fastapi.FastAPI()
 
-@app.get('/show/')
-async def selecctdb ():
-            sql = f'select * from user'
+@app.get('/show/{table}')    
+async def selecctdb(table):
+            sql = f'select * from {table}'
             mycursor.execute(sql)
             show = mycursor.fetchall()
             return show
 
-@app.get('/insert/')
-async def insert():
+@app.get('/insertus/{name},{password}/{email}/{role}')
+async def insertus(name,password,email,role):
     sql = 'insert into user values (%s, %s, %s, %s, %s)'
-    val_sql = (1,'Itui',1234,'Itui@gmail.com','customer')
+    val_sql = (None,name,password,email,role)
     mycursor.execute(sql,val_sql)
     database.mydb.commit()
     if mycursor.rowcount > 0 :
@@ -21,9 +21,41 @@ async def insert():
     else:
         return False
     
-@app.get('/delete/')
-async def dele():
-    sql = f'Delete from user where user_id = 1'
+@app.get('/isnertor/{usid}/{date}/{amount}/{sta}/{prid}')
+async def insertor(usid,date,amount,sta,prid):
+    sql = 'insert into order values (%s, %s, %s, %s, %s)'
+    val_sql = (None,usid,date,amount,sta,prid)
+    mycursor.execute(sql,val_sql)
+    database.mydb.commit()
+    if mycursor.rowcount > 0 :
+        return True
+    else:
+        return False
+
+@app.get('/insertca/{name}')
+async def insertca(name):
+    sql = 'insert into categories values (%s, %s, %s, %s, %s)'
+    val_sql = (None,name)
+    mycursor.execute(sql,val_sql)
+    database.mydb.commit()
+    if mycursor.rowcount > 0 :
+        return True
+    else:
+        return False
+@app.get('/insertva/{name}/{des}/{price}/{stock}/{id}')
+async def insertca(name,des,price,stock,id):
+    sql = 'insert into categories values (%s, %s, %s, %s, %s)'
+    val_sql = (None,name,des,price,stock,id)
+    mycursor.execute(sql,val_sql)
+    database.mydb.commit()
+    if mycursor.rowcount > 0 :
+        return True
+    else:
+        return False
+    
+@app.get('/delete/{table}/{id}')
+async def dele(table,id):
+    sql = f'Delete from {table} where user_id = {id}'
     mycursor.execute(sql)
     database.mydb.commit()
     if mycursor.rowcount > 0:
@@ -32,14 +64,14 @@ async def dele():
         return False
     
 
-@app.get('/edit/')
-async def editdb():
-    sql = f'UPDATE user SET username = "IlookAof" WHERE user_id = 1'
-
-    mycursor.execute(sql,)
+@app.get('/edit/{table}/{colum}/{idname}/{val}/{id}')
+def editdb(table,colum,idname,val,id):
+    sql = f'UPDATE {table} SET {colum} = " %s" WHERE {idname} = %s'
+    val_sql = (val,id)
+    mycursor.execute(sql,val_sql)
     database.mydb.commit()
-
     if mycursor.rowcount > 0:
         return True
     else:
         return False
+
--- realproject/dataoopapi.py
+++ realproject/dataoopapi.py
@@ -12,12 +12,14 @@ class managedb:
                 )
             self.mycursor = self.mydb.cursor()
             
+            
     @app.get('/show/{table}')       
     async def selecctdb (self,table):
             sql = f'select * from {table}'
             self.mycursor.execute(sql)
             show = self.mycursor.fetchall()
             return show
+    @app.get('/insertus/{name},{password}/{email}/{role}')
     def insertus(self,name,password,email,role):
             sql = 'insert into user values (%s ,%s ,%s ,%s ,%s)'
             val_sql = (None,name,password,email,role)
@@ -27,6 +29,7 @@ class managedb:
                 return True
             else:
                 return False
+    @app.get('/isnertor/{usid}/{date}/{amount}/{sta}/{prid}')
     def insertor(self,usid,date,amount,sta,prid):
             sql = 'insert into order  values (%s ,%s ,%s ,%s ,%s ,%s)'
             val_sql = (None,usid,date,amount,sta,prid)
@@ -36,6 +39,7 @@ class managedb:
                 return True
             else:
                 return False
+    @app.get('/insertca/{name}')
     def insertca(self,name):
             sql = 'insert into categories  values (%s ,%s)'
             val_sql = (None,name)
@@ -45,6 +49,7 @@ class managedb:
                 return True
             else:
                 return False
+    @app.get('/insertva/{name}/{des}/{price}/{stock}/{id}')
     def insertva(self,name,des,price,stock,id):
             sql = 'insert into product values (%s ,%s ,%s ,%s ,%s ,%s)'
             val_sql = (None,name,des,price,stock,id)
@@ -54,11 +59,13 @@ class managedb:
                 return True
             else:
                 return False
+    @app.get('/delete/{table}/{id}')
     def delete(self,table,id):
         sql = f'delete from {table} where user_id = {id}'
         self.mycursor.execute(sql)
         show = self.mycursor.fetchall()
         return show
+    @app.get('/edit/{table}/{colum}/{idname}/{val}/{id}')
     def editdb(self,table,colum,idname,val,id):
         sql = f'UPDATE {table} SET {colum} = " %s" WHERE {idname} = %s'
         val_sql = (val,id)
